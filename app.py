5c07ad8a230e85a0e885683ea44e33a87b12a1ce
mohamedosmann <mohamedosmann75@gmail.com>
2024-07-15 16:48:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -216,7 +216,7 @@ def home_page():
         st.markdown("</div>", unsafe_allow_html=True)
         
         st.image(load_image("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgy52YPHqzGhOKGQTNY6zN8FPyt857X73sBZv5N1LzWR97dkSPIdvrNvrlw5yPPWasoM2ZCKbviQsx_xhXro0ZOWYndFFGKFSgRrp9rqUh5_KKCsXZ9FztGCdzj8kT-LOQz2X6-OiecHZZa64IrffbagDRo9RhoFWyjkmJmBR2PIB33qsMbUuBhxAxvr-rq/s2240/food_scan_image.jpg"), caption="Dietary Tracking System")
-        st.image(load_image("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgIo6GFeyJepS6WUxR0uVGbScbCy7hWSjz_DczgokyNu0D-f7dKK3ecNfOCmMg88RJ-U0VALQ_KxDQl6iIVsiAmvGXNcmjX_s3eoZSSYMe4GUO_XoX-XF6pJ8NMqbuM30Ovq8Von6NQGSkdnv_B5DxGxBy1ySM7g_M6nMVvtXe9izzHB5LXr7OdkUKXbZ2x/s1366/homepage.png"), caption="Home Page Image")
+        st.image(load_image("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgIo6GFeyJepS6WUxR0uVGbScbCy7hWSjz_DczgokyNu0D-f7dKK3ecNfOCmMg88RJ-U0VALQ_KxDQl6iIVsiAmvGXNcmjX_s3eoZSSYMe4GUO_XoX-XF6pJ8NMqbuM30Ovq8Von6NQGSkdnv_B5DxGxBy1ySM7g_M6nMVvtXe9izzHB5LXr7OdkUKXbZ2x/s1366/homepage.png"), caption="")
 
         st.markdown("</div>", unsafe_allow_html=True)
 
